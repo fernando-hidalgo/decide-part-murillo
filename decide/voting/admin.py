46a22051f345a6347f9d49ea29894bfea2cef2c7
FernandoHA <fernan.hidalgo02@gmail.com>
2023-11-05 20:36:33 +0100

    test: Voting Admin
    Testeadas las acciones existentes y la nueva acción de exportar resultados

--- decide/voting/admin.py
+++ decide/voting/admin.py
@@ -11,48 +11,6 @@ from django.http import HttpResponse
 import io
 import json
 
-def start(modeladmin, request, queryset):
-    for v in queryset.all():
-        v.create_pubkey()
-        v.start_date = timezone.now()
-        v.save()
-
-
-def stop(ModelAdmin, request, queryset):
-    for v in queryset.all():
-        v.end_date = timezone.now()
-        v.save()
-
-
-def tally(ModelAdmin, request, queryset):
-    for v in queryset.filter(end_date__lt=timezone.now()):
-        token = request.session.get('auth-token', '')
-        v.tally_votes(token)
-
-def voting_result_data(modeladmin, request, queryset):
-    json_buffer = io.StringIO()
-    json_objects = []
-
-    for voting in queryset:
-        data = {
-            "Voting ID": voting.id,
-            "Voting Name": voting.name,
-            "Tally": voting.tally,
-            "Postproc": voting.postproc,
-        }
-
-        json_objects.append(data)
-
-    data_json = json.dumps(json_objects, indent=2)
-
-    json_buffer.write(data_json)
-
-    response = HttpResponse(json_buffer.getvalue(), content_type='text/plain')
-    response['Content-Disposition'] = 'attachment; filename="voting_result_data.json"'
-
-    return response
-
-voting_result_data.short_description = "Exportar resultados"
 
 class QuestionOptionInline(admin.TabularInline):
     model = QuestionOption
@@ -63,14 +21,57 @@ class QuestionAdmin(admin.ModelAdmin):
 
 
 class VotingAdmin(admin.ModelAdmin):
-    list_display = ('name', 'start_date', 'end_date')
-    readonly_fields = ('start_date', 'end_date', 'pub_key',
-                       'tally', 'postproc')
-    date_hierarchy = 'start_date'
+    list_display = ("name", "start_date", "end_date")
+    readonly_fields = ("start_date", "end_date", "pub_key", "tally", "postproc")
+    date_hierarchy = "start_date"
     list_filter = (StartedFilter,)
-    search_fields = ('name', )
+    search_fields = ("name",)
 
-    actions = [ start, stop, tally, voting_result_data ]
+    def start(modeladmin, request, queryset):
+        for v in queryset.all():
+            v.create_pubkey()
+            v.start_date = timezone.now()
+            v.save()
+
+    def stop(ModelAdmin, request, queryset):
+        for v in queryset.all():
+            v.end_date = timezone.now()
+            v.save()
+
+    def tally(ModelAdmin, request, queryset):
+        for v in queryset.filter(end_date__lt=timezone.now()):
+            token = request.session.get("auth-token", "")
+            v.tally_votes(token)
+
+    def voting_result_data(modeladmin, request, queryset):
+        json_buffer = io.StringIO()
+        json_objects = []
+
+        for voting in queryset:
+            data = {
+                "Voting ID": voting.id,
+                "Voting Name": voting.name,
+                "Tally": voting.tally,
+                "Postproc": voting.postproc,
+            }
+
+            json_objects.append(data)
+
+        data_json = json.dumps(json_objects, indent=2)
+
+        json_buffer.write(data_json)
+
+        response = HttpResponse(json_buffer.getvalue(), content_type="text/plain")
+        response[
+            "Content-Disposition"
+        ] = 'attachment; filename="voting_result_data.json"'
+
+        return response
+
+    voting_result_data.short_description = "Exportar resultados"
+
+    actions = [start, stop, tally, voting_result_data]
+
 
 admin.site.register(Voting, VotingAdmin)
 admin.site.register(Question, QuestionAdmin)
