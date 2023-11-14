2072cac0758184c3dbbad190900bf58313be9688
fravilpae <fravilpae@alum.us.es>
2023-11-14 13:28:48 +0100

    feat:Implementación registro basica
    Añadida la implementacion. Faltan restricciones y mejoras en cambio de vista

--- decide/authentication/views.py
+++ decide/authentication/views.py
@@ -1,14 +1,14 @@
 from rest_framework.response import Response
 from rest_framework.status import (
-        HTTP_201_CREATED,
-        HTTP_400_BAD_REQUEST,
-        HTTP_401_UNAUTHORIZED
+    HTTP_201_CREATED,
+    HTTP_400_BAD_REQUEST,
+    HTTP_401_UNAUTHORIZED,
 )
 from rest_framework.views import APIView
 from rest_framework.authtoken.models import Token
 from django.contrib.auth.models import User
 from django.db import IntegrityError
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, render
 from django.core.exceptions import ObjectDoesNotExist
 
 from .serializers import UserSerializer
@@ -16,14 +16,14 @@ from .serializers import UserSerializer
 
 class GetUserView(APIView):
     def post(self, request):
-        key = request.data.get('token', '')
+        key = request.data.get("token", "")
         tk = get_object_or_404(Token, key=key)
         return Response(UserSerializer(tk.user, many=False).data)
 
 
 class LogoutView(APIView):
     def post(self, request):
-        key = request.data.get('token', '')
+        key = request.data.get("token", "")
         try:
             tk = Token.objects.get(key=key)
             tk.delete()
@@ -35,13 +35,13 @@ class LogoutView(APIView):
 
 class RegisterView(APIView):
     def post(self, request):
-        key = request.data.get('token', '')
+        key = request.data.get("token", "")
         tk = get_object_or_404(Token, key=key)
         if not tk.user.is_superuser:
             return Response({}, status=HTTP_401_UNAUTHORIZED)
 
-        username = request.data.get('username', '')
-        pwd = request.data.get('password', '')
+        username = request.data.get("username", "")
+        pwd = request.data.get("password", "")
         if not username or not pwd:
             return Response({}, status=HTTP_400_BAD_REQUEST)
 
@@ -52,4 +52,27 @@ class RegisterView(APIView):
             token, _ = Token.objects.get_or_create(user=user)
         except IntegrityError:
             return Response({}, status=HTTP_400_BAD_REQUEST)
-        return Response({'user_pk': user.pk, 'token': token.key}, HTTP_201_CREATED)
+        return Response({"user_pk": user.pk, "token": token.key}, HTTP_201_CREATED)
+
+
+class RegisterUserView(APIView):
+    def get(self, request):
+        return render(request, "register.html")
+
+    def post(self, request):
+        username = request.data.get("username", "")
+        pwd = request.data.get("password", "")
+        email = request.data.get("email", "")
+        if not username or not pwd or not email:
+            return Response({}, status=HTTP_400_BAD_REQUEST)
+
+        try:
+
+            user = User(username=username, email=email)
+            user.set_password(pwd)
+            user.save()
+            token, _ = Token.objects.get_or_create(user=user)
+        except IntegrityError:
+            return Response({}, status=HTTP_400_BAD_REQUEST)
+        return Response({"user_pk": user.pk, "token": token.key}, HTTP_201_CREATED)
+        # return redirect('')
