7d74e6fea728e6727b552511d144d3789a63e6b1
fravilpae <fravilpae@alum.us.es>
2023-11-14 11:29:23 +0100

    feat:Creada la clase usuario
    Creada la clase usuario para usar en el formulario de registro

--- decide/authentication/models.py
+++ decide/authentication/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=100, unique=True, blank=False)
+    password = models.CharField(max_length=100, blank=False)
+    email = models.EmailField(unique=True, blank=False)
+
+    def __str__(self):
+        return self.username
+
+    def save(self):
+        self.save()
+
+    def clean(self):
+        pass
