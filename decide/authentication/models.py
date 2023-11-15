78e0197a5cf760b88db3e889be50672819b33d4c
fravilpae <fravilpae@alum.us.es>
2023-11-15 14:36:12 +0100

    feat: Eliminada clase Usuario
    Al probar la clase, he descubierto que no hacia falta crearla, se usa
    directamente la que viene.

--- decide/authentication/models.py
+++ decide/authentication/models.py
@@ -1,16 +1,3 @@
 from django.db import models
-from django.contrib.auth.password_validation import validate_password
 
-
-class User(models.Model):
-    username = models.CharField(max_length=100, unique=True, blank=False)
-    password = models.CharField(
-        max_length=100, blank=False, validators=[validate_password]
-    )
-    email = models.EmailField(unique=True, blank=False)
-
-    def __str__(self):
-        return self.username
-
-    def save(self):
-        self.save()
+# Import your models here
