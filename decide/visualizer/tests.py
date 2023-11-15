4313dfed0acf8079026be84a392d0b1c528125ea
AitorRD <aitroddue@alum.us.es>
2023-11-15 16:03:32 +0100

    feat: Interfaz traducible con i18n
    Añadidos las traducciones de texto plano y admin en Español e Ingles para i18n

--- decide/visualizer/tests.py
+++ decide/visualizer/tests.py
@@ -1,3 +1,36 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from base import mods
 
-# Create your tests here.
+import time
+import json
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+
+class Testswitchlanguage:
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        self.vars = {}
+
+    def tearDown(self):
+        self.driver.quit()
+
+    def test_testswitchlanguage(self):
+        self.driver.get("http://localhost:8000/visualizer/2/")
+        self.driver.set_window_size(945, 1016)
+        dropdown = self.driver.find_element(By.NAME, "language")
+        dropdown.find_element(By.XPATH, "//option[. = 'Inglés']").click()
+        element = self.driver.find_element(By.NAME, "language")
+        actions = ActionChains(self.driver)
+        actions.move_to_element(element).click_and_hold().perform()
+        element = self.driver.find_element(By.NAME, "language")
+        actions = ActionChains(self.driver)
+        actions.move_to_element(element).perform()
+        element = self.driver.find_element(By.NAME, "language")
+        actions = ActionChains(self.driver)
+        actions.move_to_element(element).release().perform()
