74263747b3d5d77efa9a793c5667f0be6fa30630
ramguerom <ramonguerom02@gmail.com>
2023-11-20 18:57:14 +0100

    feat comienzo creacion vista api
    Realizacion archivos comienzo vista api para el voto por si no

--- decide/voting/urls.py
+++ decide/voting/urls.py
@@ -3,6 +3,14 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.VotingView.as_view(), name='voting'),
-    path('<int:voting_id>/', views.VotingUpdate.as_view(), name='voting'),
+    path("", views.VotingView.as_view(), name="voting"),
+    path("<int:voting_id>/", views.VotingUpdate.as_view(), name="voting"),
+    path("", views.VotingView.as_view(), name="voting"),
+    path("<int:voting_id>/", views.VotingUpdate.as_view(), name="voting"),
+    path("yesno/", views.VotingYNView.as_view(), name="votingyesno"),
+    path(
+        "yesno/<int:voting_yesno_id>/",
+        views.VotingYesNoUpdate.as_view(),
+        name="votingyesno",
+    ),
 ]
