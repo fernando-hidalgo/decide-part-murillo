dcef823befe46f7fd6219ecc628ea119a6b16a33
edupizlop <edupizlop@alum.us.es>
2023-11-22 12:26:33 +0100

    feat: tarea terminada

--- decide/voting/models.py
+++ decide/voting/models.py
@@ -96,6 +96,11 @@ class Voting(models.Model):
     question = models.ManyToManyField(Question, related_name="votings")  # Cambiada a ManyToMany
     selected_option = models.ForeignKey(QuestionOption, related_name="selected_votings", null=True, blank=True,on_delete=models.SET_NULL)
     
+class Voting(models.Model):
+    name = models.CharField(max_length=200)
+    desc = models.TextField(blank=True, null=True)
+    questions = models.ManyToManyField(Question, related_name="votings")  # Cambiada a ManyToMany
+
     start_date = models.DateTimeField(blank=True, null=True)
     end_date = models.DateTimeField(blank=True, null=True)
 
@@ -142,330 +147,50 @@ class Voting(models.Model):
             votes_format = []
         return vote_list
 
-
     def tally_votes(self, token=""):
-        with transaction.atomic():
-            # Guarda la instancia de Voting antes de trabajar con las preguntas
-            self.save()
+        for question in self.questions.all():
+            selected_options = question.options.filter(voting=self)
+            votes = []
 
-            # Accede a la opción seleccionada a través del campo 'selected_option'
-            selected_option = self.selected_option
+            for option in selected_options:
+                votes.extend(option.votes.all())
 
-            if selected_option is not None:
-                selected_options = [selected_option]
-                votes = []
+            auth = self.auths.first()
+            shuffle_url = "/shuffle/{}/".format(self.id)
+            decrypt_url = "/decrypt/{}/".format(self.id)
+            auths = [{"name": a.name, "url": a.url} for a in self.auths.all()]
 
-                for option in selected_options:
-                    votes.extend(option.votes.all())
+            # first, we do the shuffle
+            data = {"msgs": votes}
+            response = mods.post(
+                "mixnet",
+                entry_point=shuffle_url,
+                baseurl=auth.url,
+                json=data,
+                response=True,
+            )
+            if response.status_code != 200:
+                # TODO: manage error
+                pass
 
-                auth = self.auths.first()
-                shuffle_url = "/shuffle/{}/".format(self.id)
-                decrypt_url = "/decrypt/{}/".format(self.id)
-                auths = [{"name": a.name, "url": a.url} for a in self.auths.all()]
-
-                # first, we do the shuffle
-                data = {"msgs": votes}
-                response = mods.post(
-                    "mixnet",
-                    entry_point=shuffle_url,
-                    baseurl=auth.url,
-                    json=data,
-                    response=True,
-                )
-                if response.status_code != 200:
-                    # TODO: manage error
-                    pass
-
-                # then, we can decrypt that
-                data = {"msgs": response.json()}
-                response = mods.post(
-                    "mixnet",
-                    entry_point=decrypt_url,
-                    baseurl=auth.url,
-                    json=data,
-                    response=True,
-                )
-
-                if response.status_code != 200:
-                    # TODO: manage error
-                    pass
-
-                # Verifica que response.json() no sea None antes de asignarlo a self.tally
-                if response.json() is not None:
-                    self.tally = response.json()
-                    self.save()
-                    self.do_postproc()
-                else:
-                    # Manejo de error: imprime o registra un mensaje de error
-                    print("Error: response.json() es None en tally_votes")
-
-            else:
-                # Si selected_option es None, asigna una lista vacía a self.tally
-                self.tally = []
-                self.save()
-                self.do_postproc()
-
-
-
-
-
-
-    def do_postproc(self):
-        tally = self.tally
-        options = self.question.first().options.all()
-
-        opts = []
-        for opt in options:
-            if isinstance(tally, list):
-                votes = tally.count(opt.number)
-            else:
-                votes = 0
-            opts.append(
-                {
-                    "option": opt.option,
-                    "number": opt.number,
-                    "votes": votes,
-                }
+            # then, we can decrypt that
+            data = {"msgs": response.json()}
+            response = mods.post(
+                "mixnet",
+                entry_point=decrypt_url,
+                baseurl=auth.url,
+                json=data,
+                response=True,
             )
 
-        data = {"escaños": self.escaños, "type": self.tallyType, "options": opts}
-        postp = mods.post("postproc", json=data)
+            if response.status_code != 200:
+                # TODO: manage error
+                pass
 
-        self.postproc = postp
-        self.save()
+            self.tally = response.json()
+            self.save()
 
-    def __str__(self):
-        return self.name
-
-
-# Modelo para votaciones de tipo si o no
-class VotingYesNo(models.Model):
-    name = models.CharField(max_length=200)
-    desc = models.TextField(blank=True, null=True)
-    question = models.ForeignKey(
-        QuestionYesNo, related_name="votingyesno", on_delete=models.CASCADE
-    )
-
-    start_date = models.DateTimeField(blank=True, null=True)
-    end_date = models.DateTimeField(blank=True, null=True)
-
-    pub_key = models.OneToOneField(
-        Key,
-        related_name="votingyesno",
-        blank=True,
-        null=True,
-        on_delete=models.SET_NULL,
-    )
-    auths = models.ManyToManyField(Auth, related_name="votingsyesno")
-
-    tallyType = models.CharField(max_length=200, default="IDENTITY")
-    escaños = models.IntegerField(default=10)
-
-    tally = JSONField(blank=True, null=True)
-    postproc = JSONField(blank=True, null=True)
-
-    def create_pubkey(self):
-        if self.pub_key or not self.auths.count():
-            return
-
-        auth = self.auths.first()
-        data = {
-            "voting": self.id,
-            "auths": [{"name": a.name, "url": a.url} for a in self.auths.all()],
-        }
-        key = mods.post("mixnet", baseurl=auth.url, json=data)
-        pk = Key(p=key["p"], g=key["g"], y=key["y"])
-        pk.save()
-        self.pub_key = pk
-        self.save()
-
-    def get_votes(self, token=""):
-        # gettings votes from store
-        votes = mods.get(
-            "store",
-            params={"voting_id": self.id},
-            HTTP_AUTHORIZATION="Token " + token,
-        )
-
-        # anon votes
-        votes_format = []
-        vote_list = []
-        for vote in votes:
-            for info in vote:
-                if info == "a":
-                    votes_format.append(vote[info])
-                if info == "b":
-                    votes_format.append(vote[info])
-            vote_list.append(votes_format)
-            votes_format = []
-        return vote_list
-
-    def tally_votes(self, token=""):
-        """
-        The tally is a shuffle and then a decrypt
-        """
-
-        votes = self.get_votes(token)
-
-        auth = self.auths.first()
-        shuffle_url = "/shuffle/{}/".format(self.id)
-        decrypt_url = "/decrypt/{}/".format(self.id)
-        auths = [{"name": a.name, "url": a.url} for a in self.auths.all()]
-
-        # first, we do the shuffle
-        data = {"msgs": votes}
-        response = mods.post(
-            "mixnet",
-            entry_point=shuffle_url,
-            baseurl=auth.url,
-            json=data,
-            response=True,
-        )
-        if response.status_code != 200:
-            # TODO: manage error
-            pass
-
-        self.tally = response.json()
-        self.save()
-
-        self.do_postproc()
-
-    def do_postproc(self):
-        tally = self.tally
-        options = self.question.pregYN.all()
-
-        opts = []
-        for opt in options:
-            if isinstance(tally, list):
-                votes = tally.count(int(opt))
-            else:
-                votes = 0
-            if int(opt) == 1:
-                opts.append({"option": "Si", "votes": votes, "number":1})
-            else:
-                opts.append({"option": "No", "votes": votes, "number":0})
-
-        data = {"escaños": self.escaños, "type": self.tallyType, "options": opts}
-        postp = mods.post("postproc", json=data)
-
-        self.postproc = postp
-        self.save()
-
-    def __str__(self):
-        return self.name
-
-
-class VotingByPreference(models.Model):
-    name = models.CharField(max_length=200)
-    desc = models.TextField(blank=True, null=True)
-    question = models.ForeignKey(
-        QuestionByPreference,
-        related_name="votingbypreference",
-        on_delete=models.CASCADE,
-    )
-
-    start_date = models.DateTimeField(blank=True, null=True)
-    end_date = models.DateTimeField(blank=True, null=True)
-
-    pub_key = models.OneToOneField(
-        Key,
-        related_name="votingbypreference",
-        blank=True,
-        null=True,
-        on_delete=models.SET_NULL,
-    )
-    auths = models.ManyToManyField(Auth, related_name="votingsbypreference")
-    
-    tallyType = models.CharField(max_length=200, default="IDENTITY")
-    escaños = models.IntegerField(default=10)
-
-    tally = JSONField(blank=True, null=True)
-    postproc = JSONField(blank=True, null=True)
-
-    def create_pubkey(self):
-        if self.pub_key or not self.auths.count():
-            return
-
-        auth = self.auths.first()
-        data = {
-            "voting": self.id,
-            "auths": [{"name": a.name, "url": a.url} for a in self.auths.all()],
-        }
-        key = mods.post("mixnet", baseurl=auth.url, json=data)
-        pk = Key(p=key["p"], g=key["g"], y=key["y"])
-        pk.save()
-        self.pub_key = pk
-        self.save()
-
-    def get_votes(self, token=""):
-        # gettings votes from store
-        auxvoting = VoteByPreference.objects.filter(voting_preference_id=self.id)
-        votes = []
-        for vote in auxvoting:
-            voting_data = {
-                "id": vote.id,
-                "voting_preference_id": vote.voting_preference_id,
-                "voter_preference_id": vote.voter_preference_id,
-                "a": vote.a,
-                "b": vote.b,
-            }
-            votes.append(voting_data)
-        # anon votes
-        votes_format = []
-        vote_list = []
-        for vote in votes:
-            for info in vote:
-                if info == "a":
-                    votes_format.append(vote[info])
-                if info == "b":
-                    votes_format.append(vote[info])
-            vote_list.append(votes_format)
-            votes_format = []
-        return vote_list
-
-    def tally_votes(self, token=""):
-        """
-        The tally is a shuffle and then a decrypt
-        """
-
-        votes = self.get_votes(token)
-
-        auth = self.auths.first()
-        shuffle_url = "/shuffle/{}/".format(self.id)
-        decrypt_url = "/decrypt/{}/".format(self.id)
-        auths = [{"name": a.name, "url": a.url} for a in self.auths.all()]
-
-        # first, we do the shuffle
-        data = {"msgs": votes}
-        response = mods.post(
-            "mixnet",
-            entry_point=shuffle_url,
-            baseurl=auth.url,
-            json=data,
-            response=True,
-        )
-        if response.status_code != 200:
-            # TODO: manage error
-            pass
-
-        # then, we can decrypt that
-        data = {"msgs": response.json()}
-        response = mods.post(
-            "mixnet",
-            entry_point=decrypt_url,
-            baseurl=auth.url,
-            json=data,
-            response=True,
-        )
-
-        if response.status_code != 200:
-            # TODO: manage error
-            pass
-
-        self.tally = response.json()
-        self.save()
-
-        self.do_postproc()
+            self.do_postproc()
 
     def do_postproc(self):
         tally = self.tally
