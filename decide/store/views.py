f3523eb2d548297b1b48615571ea0d3c230f60e7
ramguerom <ramonguerom02@gmail.com>
2023-11-26 12:36:00 +0100

    feat: Creaccion voto en store
    Creaccion del voto si no en la carpeta store

--- decide/store/views.py
+++ decide/store/views.py
@@ -5,17 +5,18 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework import generics
 
-from .models import Vote
-from .serializers import VoteSerializer
+from .models import Vote, VoteYN
+from .serializers import VoteSerializer, VoteYNSerializer
 from base import mods
 from base.perms import UserIsStaff
+from voting.models import VotingYesNo
 
 
 class StoreView(generics.ListAPIView):
     queryset = Vote.objects.all()
     serializer_class = VoteSerializer
     filter_backends = (django_filters.rest_framework.DjangoFilterBackend,)
-    filterset_fields = ('voting_id', 'voter_id')
+    filterset_fields = ("voting_id", "voter_id")
 
     def get(self, request):
         self.permission_classes = (UserIsStaff,)
@@ -24,29 +25,29 @@ class StoreView(generics.ListAPIView):
 
     def post(self, request):
         """
-         * voting: id
-         * voter: id
-         * vote: { "a": int, "b": int }
+        * voting: id
+        * voter: id
+        * vote: { "a": int, "b": int }
         """
 
-        vid = request.data.get('voting')
-        voting = mods.get('voting', params={'id': vid})
+        vid = request.data.get("voting")
+        voting = mods.get("voting", params={"id": vid})
         if not voting or not isinstance(voting, list):
             # print("por aqui 35")
             return Response({}, status=status.HTTP_401_UNAUTHORIZED)
-        start_date = voting[0].get('start_date', None)
+        start_date = voting[0].get("start_date", None)
         # print ("Start date: "+  start_date)
-        end_date = voting[0].get('end_date', None)
-        #print ("End date: ", end_date)
+        end_date = voting[0].get("end_date", None)
+        # print ("End date: ", end_date)
         not_started = not start_date or timezone.now() < parse_datetime(start_date)
-        #print (not_started)
+        # print (not_started)
         is_closed = end_date and parse_datetime(end_date) < timezone.now()
         if not_started or is_closed:
-            #print("por aqui 42")
+            # print("por aqui 42")
             return Response({}, status=status.HTTP_401_UNAUTHORIZED)
 
-        uid = request.data.get('voter')
-        vote = request.data.get('vote')
+        uid = request.data.get("voter")
+        vote = request.data.get("vote")
 
         if not vid or not uid or not vote:
             return Response({}, status=status.HTTP_400_BAD_REQUEST)
@@ -56,14 +57,18 @@ class StoreView(generics.ListAPIView):
             token = request.auth.key
         else:
             token = "NO-AUTH-VOTE"
-        voter = mods.post('authentication', entry_point='/getuser/', json={'token': token})
-        voter_id = voter.get('id', None)
+        voter = mods.post(
+            "authentication", entry_point="/getuser/", json={"token": token}
+        )
+        voter_id = voter.get("id", None)
         if not voter_id or voter_id != uid:
             # print("por aqui 59")
             return Response({}, status=status.HTTP_401_UNAUTHORIZED)
 
         # the user is in the census
-        perms = mods.get('census/{}'.format(vid), params={'voter_id': uid}, response=True)
+        perms = mods.get(
+            "census/{}".format(vid), params={"voter_id": uid}, response=True
+        )
         if perms.status_code == 401:
             # print("por aqui 65")
             return Response({}, status=status.HTTP_401_UNAUTHORIZED)
@@ -71,12 +76,88 @@ class StoreView(generics.ListAPIView):
         a = vote.get("a")
         b = vote.get("b")
 
-        defs = { "a": a, "b": b }
-        v, _ = Vote.objects.get_or_create(voting_id=vid, voter_id=uid,
-                                          defaults=defs)
+        defs = {"a": a, "b": b}
+        v, _ = Vote.objects.get_or_create(voting_id=vid, voter_id=uid, defaults=defs)
         v.a = a
         v.b = b
 
         v.save()
 
-        return  Response({})
+        return Response({})
+
+
+class StoreYNView(generics.ListAPIView):
+    queryset = VoteYN.objects.all()
+    serializer_class = VoteYNSerializer
+    filter_backends = (django_filters.rest_framework.DjangoFilterBackend,)
+    filterset_fields = ("voting_yesno_id", "voter_yesno_id")
+
+    def get(self, request):
+        self.permission_classes = (UserIsStaff,)
+        self.check_permissions(request)
+        return super().get(request)
+
+    def post(self, request):
+        """
+        * voting: id
+        * voter: id
+        * vote: { "a": int, "b": int }
+        """
+
+        vid = request.date.get("voting_yesno")
+        voting = mods.get("voting_yesno", params={"id": vid})
+        if not voting or not isinstance(voting, list):
+            return Response({}, status=status.HTTP_401_UNAUTHORIZED)
+
+        start_date = voting[0].get("start_date", None)
+        # print ("Start date: "+  start_date)
+        end_date = voting[0].get("end_date", None)
+        # print ("End date: ", end_date)
+        not_started = not start_date or timezone.now() < parse_datetime(start_date)
+        # print (not_started)
+        is_closed = end_date and parse_datetime(end_date) < timezone.now()
+        if not_started or is_closed:
+            # print("por aqui 42")
+            return Response({}, status=status.HTTP_401_UNAUTHORIZED)
+
+        uid = request.data.get("voter")
+        vote = request.data.get("vote")
+
+        if not vid or not uid or not vote:
+            return Response({}, status=status.HTTP_400_BAD_REQUEST)
+
+        # validating voter
+        if request.auth:
+            token = request.auth.key
+        else:
+            token = "NO-AUTH-VOTE"
+        voter = mods.post(
+            "authentication", entry_point="/getuser/", json={"token": token}
+        )
+
+        voter_id = voter.get("id", None)
+        if not voter_id or voter_id != uid:
+            # print("por aqui 59")
+            return Response({}, status=status.HTTP_401_UNAUTHORIZED)
+
+        # the user is in the census
+        perms = mods.get(
+            "census/{}".format(vid), params={"voter_id": uid}, response=True
+        )
+        if perms.status_code == 401:
+            # print("por aqui 65")
+            return Response({}, status=status.HTTP_401_UNAUTHORIZED)
+
+        a = vote.get("a")
+        b = vote.get("b")
+
+        defs = {"a": a, "b": b}
+        v, _ = VoteYN.objects.get_or_create(
+            voting_yesno_id=vid, voter_yesno_id=uid, defaults=defs
+        )
+        v.a = a
+        v.b = b
+
+        v.save()
+
+        return Response({})
