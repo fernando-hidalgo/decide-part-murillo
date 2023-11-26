13a26c3848f1bd08208a3a2bd6b51519fc636ada
ramguerom <ramonguerom02@gmail.com>
2023-11-26 12:12:42 +0100

    feat: continuacion votacion si no
    Continuacion del modelo y admin para la votacion de si no

--- decide/voting/admin.py
+++ decide/voting/admin.py
@@ -4,6 +4,7 @@ from django.utils import timezone
 from .models import QuestionOption
 from .models import Question
 from .models import Voting
+from .models import QuestionOptionYesNo
 from .models import QuestionYesNo
 from .models import VotingYesNo
 
@@ -33,15 +34,16 @@ class QuestionOptionInline(admin.TabularInline):
     model = QuestionOption
 
 
+class QuestionOptionYesNoInLine(admin.TabularInline):
+    model = QuestionOptionYesNo
+
+
 class QuestionAdmin(admin.ModelAdmin):
     inlines = [QuestionOptionInline]
 
 
 class QuestionYesNoAdmin(admin.ModelAdmin):
-    list_display = ("desc", "optionYes", "optionNo")
-    readonly_fields = ("optionYes", "optionNo")
-    list_filter = (StartedFilter,)
-    search_fields = ("desc",)
+    inlines = [QuestionOptionYesNoInLine]
 
 
 class VotingAdmin(admin.ModelAdmin):
@@ -50,10 +52,21 @@ class VotingAdmin(admin.ModelAdmin):
     date_hierarchy = "start_date"
     list_filter = (StartedFilter,)
     search_fields = ("name",)
+
+    actions = [start, stop, tally]
+
+
+class VotingYesNoAdmin(admin.ModelAdmin):
+    list_display = ("name", "start_date", "end_date")
+    readonly_fields = ("start_date", "end_date", "pub_key", "tally", "postproc")
+    date_hierarchy = "start_date"
+    list_filter = (StartedFilter,)
+    search_fields = ("name",)
+
     actions = [start, stop, tally]
 
 
 admin.site.register(Voting, VotingAdmin)
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(QuestionYesNo, QuestionYesNoAdmin)
-admin.site.register(VotingYesNo, VotingAdmin)
+admin.site.register(VotingYesNo, VotingYesNoAdmin)
