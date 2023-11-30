30dd2fc15570ee9648036c480a61a8c2e3be8c44
fravilpae <fravilpae@alum.us.es>
2023-11-30 17:06:11 +0100

    feat: creadas las URLS para el censo yes no
    Creadas las urls para el censo yes no en la carpeta census urls

--- decide/census/urls.py
+++ decide/census/urls.py
@@ -6,4 +6,15 @@ urlpatterns = [
     path("", views.CensusCreate.as_view(), name="census_create"),
     path("<int:voting_id>/", views.CensusDetail.as_view(), name="census_detail"),
     path("import/", views.CensusImportView.as_view(), name="import_census"),
+    path("yesno/", views.CensusYesNoCreate.as_view(), name="census_yesno_create"),
+    path(
+        "yesno/<int:voting_yesno_id>/",
+        views.CensusYesNoDetail.as_view(),
+        name="census_yesno_detail",
+    ),
+    path(
+        "yesno/import/",
+        views.CensusYesNoImportView.as_view(),
+        name="import_census_yesno",
+    ),
 ]
