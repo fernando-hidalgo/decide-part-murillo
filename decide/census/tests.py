2ca50b8d127635b635c0c6c56e583aeff2f2cb61
ursgarsan <ursgarsan@alum.us.es>
2023-12-03 13:15:29 +0100

    feat: test case de census by preference

--- decide/census/tests.py
+++ decide/census/tests.py
@@ -1,12 +1,9 @@
 import random
-from django.contrib.auth.models import User
 from django.test import TestCase
-from rest_framework.test import APIClient
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
 
 from .admin import CensusAdmin, CensusByPreferenceAdmin
 from django.http import HttpRequest
@@ -335,6 +332,75 @@ class AdminReuseCensusActionTest(TestCase):
         # Debe haber 2 censos con el mismo votante: El original y el creado reutilizando el previo
         self.assertEqual(len(Census.objects.filter(voter_id=census.voter_id)), 2)
 
+class CensusByPreferenceTestCase(BaseTestCase):
+    def setUp(self):
+        super().setUp()
+        self.census = CensusByPreference(voting_id=1, voter_id=1)
+        self.census.save()
+
+    def tearDown(self):
+        super().tearDown()
+        self.census = None
+
+    def test_check_vote_permissions(self):
+        response = self.client.get(
+            "/census/bypreference/{}/?voter_id={}".format(1, 2), format="json"
+        )
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual(response.json(), "Invalid voter")
+
+        response = self.client.get(
+            "/census/bypreference/{}/?voter_id={}".format(1, 1), format="json"
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), "Valid voter")
+
+    def test_list_voting(self):
+        response = self.client.get("/census/bypreference/?voting_id={}".format(1), format="json")
+        self.assertEqual(response.status_code, 401)
+
+        self.login(user="noadmin")
+        response = self.client.get("/census/bypreference/?voting_id={}".format(1), format="json")
+        self.assertEqual(response.status_code, 403)
+
+        self.login()
+        response = self.client.get("/census/bypreference/?voting_id={}".format(1), format="json")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {"voters": [1]})
+
+    def test_add_new_voters_conflict(self):
+        data = {"voting_id": 1, "voters": [1]}
+        response = self.client.post("/census/bypreference/", data, format="json")
+        self.assertEqual(response.status_code, 401)
+
+        self.login(user="noadmin")
+        response = self.client.post("/census/bypreference/", data, format="json")
+        self.assertEqual(response.status_code, 403)
+
+        self.login()
+        response = self.client.post("/census/bypreference/", data, format="json")
+        self.assertEqual(response.status_code, 409)
+
+    def test_add_new_voters(self):
+        data = {"voting_id": 2, "voters": [1, 2, 3, 4]}
+        response = self.client.post("/census/bypreference/", data, format="json")
+        self.assertEqual(response.status_code, 401)
+
+        self.login(user="noadmin")
+        response = self.client.post("/census/bypreference/", data, format="json")
+        self.assertEqual(response.status_code, 403)
+
+        self.login()
+        response = self.client.post("/census/bypreference/", data, format="json")
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(len(data.get("voters")), CensusByPreference.objects.count() - 1)
+
+    def test_destroy_voter(self):
+        data = {"voters": [1], "voting_id": 1}
+        response = self.client.delete("/census/bypreference/{}/".format(1), data, format="json")
+        self.assertEqual(response.status_code, 204)
+        self.assertEqual(0, CensusByPreference.objects.count())
+
 class CensusByPreferenceTest(StaticLiveServerTestCase):
     def setUp(self):
         # Load base test functionality for decide
@@ -569,4 +635,3 @@ class ByPreferenceAdminReuseCensusActionTest(TestCase):
 
         # Debe haber 2 censos con el mismo votante: El original y el creado reutilizando el previo
         self.assertEqual(len(CensusByPreference.objects.filter(voter_id=census.voter_id)), 2)
-
--- decide/census/urls.py
+++ decide/census/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path("", views.CensusCreate.as_view(), name="census_create"),
     path("bypreference/", views.CensusByPreferenceCreate.as_view(), name="census_by_preference_create"),
     path("<int:voting_id>/", views.CensusDetail.as_view(), name="census_detail"),
-    path("bypreference/<int:voting_by_preference_id>/", views.CensusByPreferenceDetail.as_view(), name="census_by_preference_detail"),
+    path("bypreference/<int:voting_id>/", views.CensusByPreferenceDetail.as_view(), name="census_by_preference_detail"),
     path("import/", views.CensusImportView.as_view(), name="import_census"),
     path("bypreference/import/", views.CensusImportView.as_view(), name="import_census_by_preference"),
 ]
