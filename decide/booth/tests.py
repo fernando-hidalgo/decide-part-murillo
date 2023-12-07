cf37f7d97e96f66504a6af1dd216b0e47cf6e306
Jeszamgue <jeszamgue@alum.us.es>
2023-12-07 19:10:36 +0100

    fix : Corregido nombre de test en booth
    Cambiado el nombre de los dos últimos test del booth

--- decide/booth/tests.py
+++ decide/booth/tests.py
@@ -24,13 +24,13 @@ class BoothTestCase(BaseTestCase):
         response = self.client.get("/booth/10000")
         self.assertEqual(response.status_code, 301)
 
-    def testBoothNotFound(self):
+    def testBoothPreferenceNotFound(self):
 
         # Se va a probar con el numero 10000 pues en las condiciones actuales en las que nos encontramos no parece posible que se genren 10000 votaciones diferentes
         response = self.client.get("/booth/preference/10000/")
         self.assertEqual(response.status_code, 404)
 
-    def testBoothRedirection(self):
+    def testBoothPreferenceRedirection(self):
 
         # Se va a probar con el numero 10000 pues en las condiciones actuales en las que nos encontramos no parece posible que se genren 10000 votaciones diferentes
         response = self.client.get("/booth/preference/10000")
