9235f8a961b0d8169040dd2c28f47c1cf461894e
Eduardo Pizarro López <edupizlop@alum.us.es>
2023-12-11 13:39:08 +0100

    fix: cambio de errores en codacy

--- decide/voting/models.py
+++ decide/voting/models.py
@@ -4,6 +4,8 @@ from django.db import transaction
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.db import transaction
+from django.dispatch import receiver
+
 
 
 from base import mods
