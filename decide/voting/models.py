1af96f3bbb9573a3498ce204845d73dcdb139c95
paupenfer1 <paaulap02@gmail.com>
2023-12-15 00:48:13 +0100

    feat: Añadido el contenido al archivo models de voting

--- decide/voting/models.py
+++ decide/voting/models.py
@@ -19,6 +19,12 @@ class Question(models.Model):
     def __str__(self):
         return self.desc
 
+class QuestionMultiChoice(models.Model):
+    desc = models.TextField()
+
+    def __str__(self):
+        return self.desc
+
 
 # Modelo para preguntas de tipo si o no
 class QuestionYesNo(models.Model):
@@ -47,6 +53,23 @@ class QuestionOption(models.Model):
         if not self.number:
             self.number = self.question.options.count() + 1
         super(QuestionOption, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return "{} ({})".format(self.option, self.number)
+
+class QuestionOptionMultiChoice(models.Model):
+    question = models.ForeignKey(
+        QuestionMultiChoice, related_name="multichoices", on_delete=models.CASCADE
+    )
+    number = models.PositiveIntegerField(blank=True, null=True)
+    option = models.TextField()
+    multichoice = models.PositiveIntegerField(blank=True, null= True)
+
+    def save(self):
+        self.multichoice = 0
+        if not self.number:
+            self.number = self.question.multichoices.count() + 2
+        return super().save()
 
     def __str__(self):
         return "{} ({})".format(self.option, self.number)
@@ -497,6 +520,143 @@ class VotingByPreference(models.Model):
 
         self.postproc = postp
         self.save()
+
+    def __str__(self):
+        return self.name
+
+
+class VotingMultiChoice(models.Model):
+    name = models.CharField(max_length=200)
+    desc = models.TextField(blank=True, null=True)
+    question = models.ForeignKey(
+        QuestionMultiChoice, related_name="votingmultichoice", on_delete=models.CASCADE
+    )
+
+    start_date = models.DateTimeField(blank=True, null=True)
+    end_date = models.DateTimeField(blank=True, null=True)
+
+    pub_key = models.OneToOneField(
+        Key, related_name="votingmultichoice",
+        blank=True, 
+        null=True, 
+        on_delete=models.SET_NULL
+    )
+    auths = models.ManyToManyField(Auth, related_name="votingsmultichoice")
+
+    tallyType = models.CharField(max_length=200, default="IDENTITY")
+    escaños = models.IntegerField(default=10)
+    tally = JSONField(blank=True, null=True)
+    postproc = JSONField(blank=True, null=True)
+
+    def create_pubkey(self):
+        if self.pub_key or not self.auths.count():
+            return
+
+        auth = self.auths.first()
+        data = {
+            "voting": self.id,
+            "auths": [{"name": a.name, "url": a.url} for a in self.auths.all()],
+        }
+        key = mods.post("mixnet", baseurl=auth.url, json=data)
+        pk = Key(p=key["p"], g=key["g"], y=key["y"])
+        pk.save()
+        self.pub_key = pk
+        self.save()
+
+    def get_votes(self, token=""):
+        # gettings votes from store
+        votes = mods.get(
+            "store", params={"voting_id": self.id}, HTTP_AUTHORIZATION="Token " + token
+        )
+        # anon votes
+        votes_format = []
+        vote_list = []
+        for vote in votes:
+            for info in vote:
+                if info == "a":
+                    votes_format.append(vote[info])
+                if info == "b":
+                    votes_format.append(vote[info])
+            vote_list.append(votes_format)
+            votes_format = []
+        return vote_list
+
+    def tally_votes(self, token=""):
+        """
+        The tally is a shuffle and then a decrypt
+        """
+
+        votes = self.get_votes(token)
+
+        auth = self.auths.first()
+        shuffle_url = "/shuffle/{}/".format(self.id)
+        decrypt_url = "/decrypt/{}/".format(self.id)
+        auths = [{"name": a.name, "url": a.url} for a in self.auths.all()]
+
+        # first, we do the shuffle
+        data = {"msgs": votes}
+        response = mods.post(
+            "mixnet",
+            entry_point=shuffle_url,
+            baseurl=auth.url,
+            json=data,
+            response=True,
+        )
+        if response.status_code != 200:
+            # TODO: manage error
+            pass
+
+        # then, we can decrypt that
+        data = {"msgs": response.json()}
+        response = mods.post(
+            "mixnet",
+            entry_point=decrypt_url,
+            baseurl=auth.url,
+            json=data,
+            response=True,
+        )
+
+        if response.status_code != 200:
+            # TODO: manage error
+            pass
+
+        self.tally = response.json()
+        self.save()
+
+        self.do_postproc()
+
+    def do_postproc(self):
+        tally = self.tally
+        options = self.question.multichoices.all()
+
+        opts = []
+        for opt in options:
+            if isinstance(tally, list):
+                votes = tally.count(opt.number)
+            else:
+                votes = 0
+            opts.append(
+                {
+                    "option": opt.option,
+                    "number": opt.number,
+                    "votes": votes,
+                }
+            )
+
+        data = {"escaños": self.escaños, "type": self.tallyType, "options": opts}
+        postp = mods.post("postproc", json=data)
+
+        self.postproc = postp
+        self.save()
+    
+    def vote_multi_choice(self, selected_options):
+        """
+        Permite a los usuarios votar por múltiples opciones.
+        """
+        for option_id in selected_options:
+            option = self.multi_options.get(pk=option_id)
+            option.votes += 1
+            option.save()
 
     def __str__(self):
         return self.name
