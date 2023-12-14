fa524bb249b5651596e21016159a0f47c48de60f
ramguerom <ramonguerom02@gmail.com>
2023-12-14 09:11:35 +0100

    feat: Comienzo visualizer yesno
    Comienzo del visualizer de la votacion yesno

--- decide/visualizer/urls.py
+++ decide/visualizer/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import VisualizerView, VisualizerViewPreference
+from .views import VisualizerView, VisualizerViewPreference, VisualizerViewYesNo
 
 
 urlpatterns = [
     path("<int:voting_id>/", VisualizerView.as_view()),
     path("preference/<int:voting_preference_id>/", VisualizerViewPreference.as_view()),
+    path("yesno/<int:voting_yesno_id>/", VisualizerViewYesNo.as_view()),
 ]
