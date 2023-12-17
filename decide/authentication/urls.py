1a6195ed20de4d154eaa623866045af4747c82d4
[[iniruimar]] <[[iniruimar@alum.us.es]]>
2023-12-17 20:05:35 +0100

    feat: Añadido los cambios en urls de autenticacion para el logout

--- decide/authentication/urls.py
+++ decide/authentication/urls.py
@@ -1,12 +1,11 @@
 from django.urls import include, path
 from rest_framework.authtoken.views import obtain_auth_token
-
-from .views import GetUserView, LogoutView, RegisterView, RegisterUserView, UserList, LoginUserView
+from .views import GetUserView, RegisterView, RegisterUserView, UserList, LoginUserView, LogoutUserView
 
 
 urlpatterns = [
     path("login/", obtain_auth_token),
-    path("logout/", LogoutView.as_view()),
+    path("logout/", LogoutUserView.as_view(), name="logout"),
     path("getuser/", GetUserView.as_view()),
     path("register/", RegisterView.as_view()),
     path("user/", UserList.as_view(), name="user-list"),
